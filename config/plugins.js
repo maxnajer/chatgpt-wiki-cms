cd6a43a04a0765f95ae2983756766491bd862c93
maxnajer <maximonajera@protonmail.com>
2025-06-13 06:08:03 -0600

    feat: configure users-permissions jwtSecret

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,7 @@
 // path: ./config/plugins.js
 module.exports = ({ env }) => ({
-  // Configure the users-permissions plugin
   'users-permissions': {
     config: {
-      // Read JWT_SECRET from process.env
       jwtSecret: env('JWT_SECRET'),
     },
   },
